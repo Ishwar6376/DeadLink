277eb19bcf99710801de122b230e1fcdc3a0c3cf
Ishwar Kumawat <ishwarkumawat156@gmail.com>
2025-12-03 21:28:45 +0530

    shift backend to ts

--- server/routes/userRoute.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { create} from "../controllers/userController.js";
-
-const router = express.Router();
-
-router.post("/user", create);
-
-export default router;
-
---
+++ server/src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+const PORT:Number=3000;
+
+app.get('/', (req, res) => {
+    res.send('Welcome to typescript backend!');
+})
+
+// Server setup
+app.listen(PORT,() => {
+    console.log('The application is listening '
+          + 'on port http://localhost/'+PORT);
+})
