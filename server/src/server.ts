b381d1d94ea0643c811f1b048c06b66e462fbfaf
Ishwar Kumawat <ishwarkumawat156@gmail.com>
2025-12-03 23:11:50 +0530

    fix some bugs

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,11 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
-const PORT:Number=3000;
 
-app.get('/', (req, res) => {
-    res.send('Welcome to typescript backend!');
-})
 
-// Server setup
-app.listen(PORT,() => {
-    console.log('The application is listening '
-          + 'on port http://localhost/'+PORT);
-})
+
+
+
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000");
+});
